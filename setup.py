bc94e0f68673eb29ce87c5e69fb727a102e5b8d4
warmonger <d>
2014-12-29 11:21:47 +0600

    Create package from code

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='feedlyclient',
+    version='0.0.1',
+    url='',
+    license='',
+    packages=find_packages(),
+    author='zgw21cn',
+    author_email='',
+    description='Client for Feedly ',
+    include_package_data=True,
+    install_requires=[
+
+    ],
+)
