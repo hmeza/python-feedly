b676170aa80a71a4d0a262e4ad54d37df6603d10
warmonger <d>
2015-02-03 21:32:39 +0600

    Ver 0.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload -r pypi')
     sys.exit()
 
-PACKAGE_VERSION = '0.0.3'
+PACKAGE_VERSION = '0.0.4'
 PACKAGE_DOWNLOAD_URL = (
 
 )
